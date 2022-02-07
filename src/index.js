099d0235bcf793401cc90dd8aa84ffbb71664e9d
shadyagmy <72800568+ShadyAgamy@users.noreply.github.com>
2022-02-07 12:03:38 +0200

    upload homepage  to github

--- src/index.js
+++ src/index.js
@@ -10,5 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-// https://git-codecommit.eu-central-1.amazonaws.com/v1/repos/zajil
